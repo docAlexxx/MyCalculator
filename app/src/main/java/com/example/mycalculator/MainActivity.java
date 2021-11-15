6e9f4c7f2d7c8b142b6b1cb988bdca33b2571b55
docAlexxx <docalex@mail.ru>
2021-11-15 14:06:48 +0400

    init Elements

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText numbersEditText;
-    private EditText actionEditText;
+    private TextView numbersTextView;
+    private TextView processTextView;
     private MaterialButton number0Button;
     private MaterialButton number1Button;
     private MaterialButton number2Button;
@@ -24,12 +25,11 @@ public class MainActivity extends AppCompatActivity {
     private MaterialButton minusButton;
     private MaterialButton multiplyButton;
     private MaterialButton devideButton;
-    private MaterialButton dotButton;
-    private MaterialButton plusMinusChangeButton;
+    private MaterialButton pointButton;
+    private MaterialButton signChangeButton;
     private MaterialButton resultButton;
     private MaterialButton deleteButton;
     private MaterialButton resetButton;
-    private MaterialButton settingButton;
     Calculator calculator = new Calculator();
 
 
@@ -37,5 +37,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initElements();
     }
+
+    private void initElements() {
+        numbersTextView = findViewById(R.id.number_screen);
+        processTextView = findViewById(R.id.process_screen);
+        number0Button = findViewById(R.id.zero_button);
+        number1Button = findViewById(R.id.one_button);
+        number2Button = findViewById(R.id.two_button);
+        number3Button = findViewById(R.id.three_button);
+        number4Button = findViewById(R.id.four_button);
+        number5Button = findViewById(R.id.five_button);
+        number6Button = findViewById(R.id.six_button);
+        number7Button = findViewById(R.id.seven_button);
+        number8Button = findViewById(R.id.eight_button);
+        number9Button = findViewById(R.id.nine_button);
+        plusButton = findViewById(R.id.plus_button);
+        minusButton = findViewById(R.id.minus_button);
+        multiplyButton = findViewById(R.id.multiply_button);
+        devideButton = findViewById(R.id.divide_button);
+        pointButton = findViewById(R.id.point_button);
+        signChangeButton = findViewById(R.id.change_number_sign_button);
+        resultButton = findViewById(R.id.result_button);
+        deleteButton = findViewById(R.id.delete_button);
+        resetButton = findViewById(R.id.reset_button);
+    }
+
 }
